a49a43aae35e5f95c6f801b9d5d949fd71400c02
yuvaravii <gceravishankar@gmail.com>
2024-11-26 16:30:27 +0530

    new file

--- MyPythonNotesPlayGround/16 Flask Framework/flask/app3.py
+++ MyPythonNotesPlayGround/16 Flask Framework/flask/app3.py
@@ -18,8 +18,41 @@ def home():
 
 ## get all the items within the app
 @app.route("/items",methods =["GET"])
-def get_items():
+def get_all_items():
     return jsonify(items_dict_list)
 
-if __name__ == "main":
-    app.run(debug = True)
+## get the items with respect id requested 
+@app.route("/items/<int:item_id>",methods = ["GET"])
+def get_id_specific_item(item_id):
+    id_specific_item = next((item for item in items_dict_list if item["id"] == item_id),None)
+    if id_specific_item is None:
+        return jsonify({"error":"item is missing"}) 
+    else: 
+        return jsonify(id_specific_item)
+    
+## writing scripts for creating new item list
+@app.route("/items", methods = ["POST"])
+def create_item():
+    if not request.json or not "name" in request.json:
+        return jsonify({"error":"item is missing"}) 
+    else:
+        new_item = {
+            "id" : items_dict_list[-1]["id"] if items_dict_list else 1,
+            "name" : request.json["name"],
+            "description" : request.json["description"]
+            }
+    items_dict_list.append(new_item)
+    return jsonify(new_item)
+
+## put method is used for updating the existing value within the dictionary
+@app.route("/items/<int:item_id>",methods = ["PUT"])
+def update_item(item_id):
+    fetched_item = next((item for item in items_dict_list if item["id"] == item_id),None)
+    if fetched_item is None:
+        return jsonify({"error":"Item is missing"})
+    fetched_item["name"] = request.json.get("name", items_dict_list["name"])
+
+    return jsonify(fetched_item)
+
+if __name__ == "__main__":
+    app.run(debug=True)
